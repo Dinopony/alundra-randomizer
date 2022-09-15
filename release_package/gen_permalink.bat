5c216a3638a140105b4ca9a38821b915555a275f
Dinopony <dinopony.gg@gmail.com>
2022-09-16 00:01:16 +0200

    Added stuff for release package

---
+++ release_package/gen_permalink.bat
@@ -0,0 +1,3 @@
+cd /D "%~dp0"
+alundra-randomizer.exe --outputrom=./seeds/ --permalink --nopause
+pause
---
+++ release_package/gen_preset.bat
@@ -0,0 +1,3 @@
+cd /D "%~dp0"
+alundra-randomizer.exe --outputrom=./seeds/ --preset=./presets/default.json --nopause
+pause
