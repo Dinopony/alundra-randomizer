9e1de91e63c3def87fffcb80c762c7f4aa2fb174
Dinopony <dinopony.gg@gmail.com>
2022-09-16 09:59:48 +0200

    Fixed a quirk preventing compilation on GCC

--- src/tools/exception.hpp
+++ src/tools/exception.hpp
@@ -3,7 +3,15 @@
 #include <stdexcept>
 
 class RandomizerException : public std::exception {
+private:
+    std::string _reason;
+
 public:
-    explicit RandomizerException(const std::string& reason) : std::exception(reason.c_str())
+    explicit RandomizerException(const std::string& reason) : std::exception()
     {}
+
+    [[nodiscard]] const char* what() const override
+    {
+        return _reason.c_str();
+    }
 };
