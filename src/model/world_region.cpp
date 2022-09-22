3be53c6ebca484e06fd8c9e21669fbbf3ef61472
Dinopony <dinopony.gg@gmail.com>
2022-09-22 14:27:24 +0200

    Removed an unused attribute of WorldRegion

--- src/model/world_region.cpp
+++ src/model/world_region.cpp
@@ -4,11 +4,11 @@
 #include "world_node.hpp"
 #include "item_source.hpp"
 
-WorldRegion::WorldRegion(std::string name, std::string hint_name, std::vector<WorldNode*> nodes, std::vector<uint16_t> dark_map_ids, bool can_be_hinted_as_required) :
+WorldRegion::WorldRegion(std::string name, std::string hint_name, 
+                         std::vector<WorldNode*> nodes, bool can_be_hinted_as_required) :
     _name                       (std::move(name)),
     _hint_name                  (std::move(hint_name)),
     _nodes                      (std::move(nodes)),
-    _dark_map_ids               (std::move(dark_map_ids)),
     _can_be_hinted_as_required  (can_be_hinted_as_required)
 {
     for(WorldNode* node : _nodes)
@@ -25,9 +25,6 @@ Json WorldRegion::to_json() const
     json["nodeIds"] = Json::array();
     for(WorldNode* node : _nodes)
         json["nodeIds"].emplace_back(node->id());
-
-    if(!_dark_map_ids.empty())
-        json["dark_map_ids"] = _dark_map_ids;
 
     return json;
 }
@@ -46,11 +43,6 @@ WorldRegion* WorldRegion::from_json(const Json& json, const std::map<std::string
     for(std::string node_id : json.at("nodeIds"))
         nodes.emplace_back(all_nodes.at(node_id));
 
-    std::vector<uint16_t> dark_map_ids;
-    if(json.contains("darkMapIds")) 
-        json.at("darkMapIds").get_to(dark_map_ids);
-
-    return new WorldRegion(name, hint_name, nodes, dark_map_ids, can_be_hinted_as_required);
 }
 
 std::map<std::string, ItemSource*> WorldRegion::item_sources() const
@@ -63,4 +55,5 @@ std::map<std::string, ItemSource*> WorldRegion::item_sources() const
     }
 
     return item_sources;
+    return new WorldRegion(name, hint_name, nodes, can_be_hinted_as_required);
 }
--- src/model/world_region.hpp
+++ src/model/world_region.hpp
@@ -14,19 +14,15 @@ private:
     std::string _name;
     std::string _hint_name;
     std::vector<WorldNode*> _nodes;
-    std::vector<uint16_t> _dark_map_ids;
     bool _can_be_hinted_as_required;
 
 public:
-    WorldRegion(std::string name, std::string hint_name,
-                std::vector<WorldNode*> nodes, std::vector<uint16_t> dark_map_ids,
-                bool can_be_hinted_as_required);
+    WorldRegion(std::string name, std::string hint_name, 
+                std::vector<WorldNode*> nodes, bool can_be_hinted_as_required);
 
     [[nodiscard]] const std::string& name() const { return _name; }
     [[nodiscard]] const std::string& hint_name() const { return _hint_name; }
     [[nodiscard]] const std::vector<WorldNode*>& nodes() const { return _nodes; }
-    [[nodiscard]] const std::vector<uint16_t>& dark_map_ids() const { return _dark_map_ids; }
-
     [[nodiscard]] bool can_be_hinted_as_required() const { return _can_be_hinted_as_required; }
     [[nodiscard]] std::map<std::string, ItemSource*> item_sources() const;
 
