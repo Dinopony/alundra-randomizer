d6fb1fbe6b09778fbb80b37c50d6a8e4849d1790
Dinopony <dinopony.gg@gmail.com>
2022-09-25 19:58:22 +0200

    Improved Cephas removal, should help reducing lag in C2

--- src/patches/patch_remove_useless_cephas.hpp
+++ src/patches/patch_remove_useless_cephas.hpp
@@ -14,59 +14,37 @@ class PatchRemoveUselessCephas : public GamePatch
 public:
     void alter_datas_file(BinaryFile& data, const GameData& game_data, const RandomizerWorld& world) override
     {
-        remove_cephas(data, 0x3664A6);
-        remove_cephas(data, 0x3664CE);
-        remove_cephas(data, 0x3664E2);
-        remove_cephas(data, 0x3664F6);
-        remove_cephas(data, 0x36650A);
-        remove_cephas(data, 0x36651E);
-        remove_cephas(data, 0x3665FA);
-        remove_cephas(data, 0x36660E);
-        remove_cephas(data, 0x366622);
-        remove_cephas(data, 0x366636);
-        remove_cephas(data, 0x36664A);
-        remove_cephas(data, 0x36665E);
-        remove_cephas(data, 0x366672);
-        remove_cephas(data, 0x366686);
-        remove_cephas(data, 0x366AAA);
-        remove_cephas(data, 0x366C8A);
-        remove_cephas(data, 0x366CC6);
-        remove_cephas(data, 0x366CDA);
-        remove_cephas(data, 0x366CEE);
-        remove_cephas(data, 0x366D02);
+        const std::vector<uint8_t> ALWAYS_BRANCH = { 0x31, 0x00, 0x00 };
 
-        remove_cephas(data, 0x44591CA);
-        remove_cephas(data, 0x4459206);
-        remove_cephas(data, 0x49505DA);
-        remove_cephas(data, 0x4950602);
-        remove_cephas(data, 0x4950616);
-        remove_cephas(data, 0x495062A);
-        remove_cephas(data, 0x495063E);
-        remove_cephas(data, 0x4950652);
-        remove_cephas(data, 0x495072E);
-        remove_cephas(data, 0x4950742);
-        remove_cephas(data, 0x4950756);
-        remove_cephas(data, 0x495076A);
-        remove_cephas(data, 0x495077E);
-        remove_cephas(data, 0x4950792);
-        remove_cephas(data, 0x49507A6);
-        remove_cephas(data, 0x49507BA);
-        remove_cephas(data, 0x4950BDE);
-        remove_cephas(data, 0x4950DBE);
-        remove_cephas(data, 0x4950DFA);
-        remove_cephas(data, 0x4950E0E);
-        remove_cephas(data, 0x4950E22);
-        remove_cephas(data, 0x4950E36);
-    }
+        // Base variant
+//        data.set_bytes(0x364B75, ALWAYS_BRANCH);      // Flag D0
+        data.set_bytes(0x364B7D, ALWAYS_BRANCH);      // Flag D1
+        data.set_bytes(0x364B85, ALWAYS_BRANCH);      // Flag D2
+        data.set_bytes(0x364B8D, ALWAYS_BRANCH);      // Flag D3
+        data.set_bytes(0x364B95, ALWAYS_BRANCH);      // Flag D4
+        data.set_bytes(0x364B9D, ALWAYS_BRANCH);      // Flag D5
+        data.set_bytes(0x364BA5, ALWAYS_BRANCH);      // Flag D6
+        data.set_bytes(0x364BAD, ALWAYS_BRANCH);      // Flag D7
+        data.set_bytes(0x364BCE, ALWAYS_BRANCH);      // Flag D9
+        data.set_bytes(0x364BD6, ALWAYS_BRANCH);      // Flag DA
+        data.set_bytes(0x364BDE, ALWAYS_BRANCH);      // Flag DB
+        data.set_bytes(0x364BE6, ALWAYS_BRANCH);      // Flag DC
+        data.set_bytes(0x364BEE, ALWAYS_BRANCH);      // Flag DD
 
-private:
-    static void remove_cephas(BinaryFile& data, uint32_t entity_base_address)
-    {
-        // Position Cephas outside of the map
-        data.set_word(entity_base_address + 5, 0xFF00);
-
-        // Remove events linked with that Cephas instance
-        data.set_byte(entity_base_address + 10, 0x00);
+        // Other variant
+//        data.set_bytes(0x494ECA9, ALWAYS_BRANCH);      // Flag D0
+        data.set_bytes(0x494ECB1, ALWAYS_BRANCH);      // Flag D1
+        data.set_bytes(0x494ECB9, ALWAYS_BRANCH);      // Flag D2
+        data.set_bytes(0x494ECC1, ALWAYS_BRANCH);      // Flag D3
+        data.set_bytes(0x494ECC9, ALWAYS_BRANCH);      // Flag D4
+        data.set_bytes(0x494ECD1, ALWAYS_BRANCH);      // Flag D5
+        data.set_bytes(0x494ECD9, ALWAYS_BRANCH);      // Flag D6
+        data.set_bytes(0x494ECE1, ALWAYS_BRANCH);      // Flag D7
+        data.set_bytes(0x494ED02, ALWAYS_BRANCH);      // Flag D9
+        data.set_bytes(0x494ED0A, ALWAYS_BRANCH);      // Flag DA
+        data.set_bytes(0x494ED12, ALWAYS_BRANCH);      // Flag DB
+        data.set_bytes(0x494ED1A, ALWAYS_BRANCH);      // Flag DC
+        data.set_bytes(0x494ED22, ALWAYS_BRANCH);      // Flag DD
     }
 };
 
