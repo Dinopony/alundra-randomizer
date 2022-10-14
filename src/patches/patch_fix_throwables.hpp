671e8c42bb6622ba35b587c0de5520ca4d81239b
Dinopony <dinopony.gg@gmail.com>
2022-10-14 20:42:41 +0200

    Added shadows to shop items

--- src/patches/patch_fix_throwables.hpp
+++ src/patches/patch_fix_throwables.hpp
@@ -22,8 +22,8 @@ public:
         data.set_byte(0x1EBFA10, 0x00);
 
         // Get pointer offsets for valid item (Life Vessel)
-        constexpr uint32_t STARTING_ADDR = 0x830;
-        uint32_t life_vessel_offset_addr = STARTING_ADDR + ((ITEM_LIFE_VESSEL + 0x1E) * 4);
+        constexpr uint32_t STARTING_ADDR = 0x8a8;
+        uint32_t life_vessel_offset_addr = STARTING_ADDR + (ITEM_LIFE_VESSEL * 4);
         uint32_t life_vessel_offset = data.get_long_le(life_vessel_offset_addr);
         uint32_t life_vessel_ptr_table = 0x800 + life_vessel_offset;
         uint32_t life_vessel_ptr_1 = data.get_long(life_vessel_ptr_table); // 5C300300
@@ -31,11 +31,14 @@ public:
 
         for(uint16_t item_id = 0x1 ; item_id < ITEM_COUNT ; ++item_id)
         {
-            uint32_t addr = STARTING_ADDR + ((item_id + 0x1E) * 4);
-
-            uint32_t pointer_table_offset = data.get_long_le(addr) + 0x800;
-            data.set_long(pointer_table_offset, life_vessel_ptr_1);
-            data.set_long(pointer_table_offset + 4, life_vessel_ptr_2);
+            uint32_t ptr_addr = STARTING_ADDR + (item_id * 4);
+            uint32_t addr = 0x800 + data.get_long_le(ptr_addr);
+            // Add shadows and give the same properties to all item entities
+            data.set_word(addr + 0x18, 0xF6F9);
+            data.set_word(addr + 0x1B, 0x150F);
+            // Prevent crashes when throwing items
+            data.set_long(addr, life_vessel_ptr_1);
+            data.set_long(addr + 4, life_vessel_ptr_2);
         }
     }
 };
